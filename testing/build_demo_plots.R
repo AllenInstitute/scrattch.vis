92163b49d54e038964942bb67a00f76e90efb05e
Lucas Graybuck <lucasg@alleninstitute.org>
2018-07-30 09:07:28 -0700

    Adding demo plot script

---
+++ testing/build_demo_plots.R
@@ -0,0 +1,72 @@
+devtools::load_all()
+
+sample_bar_plot(data_df, 
+                 anno, 
+                 genes = c("Pvalb","Sst","Rorb"), 
+                 grouping = "primary_type", 
+                 log_scale = FALSE,
+                 font_size = 5)
+ggsave("man/figures/sample_bar_plot.png", width = 4, height = 2.5)
+
+sample_fire_plot(data_df, 
+                  anno, 
+                  genes = c("Pvalb","Sst","Rorb"), 
+                  grouping = "primary_type", 
+                  log_scale = TRUE,
+                  top_value = "lowest",
+                 font_size = 5)
+ggsave("man/figures/sample_fire_plot.png", width = 4, height = 2.5)
+
+sample_heatmap_plot(data_df, 
+                 anno, 
+                 genes = c("Pvalb","Sst","Rorb"), 
+                 grouping = "primary_type", 
+                 log_scale = TRUE,
+                 font_size = 5)
+ggsave("man/figures/sample_heatmap_plot.png", width = 4, height = 2.5)
+
+group_dot_plot(data_df, 
+                    anno, 
+                    genes = c("Pvalb","Sst","Rorb"), 
+                    grouping = "primary_type", 
+                    log_scale = TRUE,
+                    font_size = 5,
+               rotate_counts = TRUE)
+ggsave("man/figures/group_dot_plot.png", width = 4, height = 2.5)
+
+group_violin_plot(data_df, 
+               anno, 
+               genes = c("Pvalb","Sst","Rorb"), 
+               grouping = "primary_type", 
+               log_scale = FALSE,
+               font_size = 5,
+               rotate_counts = TRUE)
+ggsave("man/figures/group_violin_plot.png", width = 4, height = 2.5)
+
+group_quasirandom_plot(data_df, 
+                  anno, 
+                  genes = c("Pvalb","Sst","Rorb"), 
+                  grouping = "primary_type", 
+                  log_scale = FALSE,
+                  font_size = 5,
+                  rotate_counts = TRUE)
+ggsave("man/figures/group_quasirandom_plot.png", width = 4, height = 2.5)
+
+group_box_plot(data_df, 
+                  anno, 
+                  genes = c("Pvalb","Sst","Rorb"), 
+                  grouping = "primary_type", 
+                  log_scale = FALSE,
+                  font_size = 5,
+               rotate_counts = TRUE)
+ggsave("man/figures/group_box_plot.png", width = 4, height = 2.5)
+
+group_heatmap_plot(data_df, 
+               anno, 
+               genes = c("Pvalb","Sst","Rorb"), 
+               grouping = "primary_type", 
+               stat = "tmean",
+               log_scale = TRUE,
+               font_size = 5,
+               rotate_counts = TRUE)
+ggsave("man/figures/group_heatmap_plot.png", width = 4, height = 2.5)

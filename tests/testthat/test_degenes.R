f52786b3d9b2eac8e768a8940d2bd672028e4dfb
Adriana <adrianas@alleninstitute.org>
2018-09-26 12:21:57 -0700

    adding more tests

---
+++ tests/testthat/test_degenes.R
@@ -0,0 +1,7 @@
+library(scrattch.vis)
+
+context("DE genes")
+
+test_that("Find sparse, differentially-expressed genes in pairwise comparisons", {
+  skip()
+})
---
+++ tests/testthat/test_filtering.R
@@ -0,0 +1,7 @@
+library(scrattch.vis)
+
+context("Filtering")
+
+test_that("Filter a data.frame with parameters provided in a list", {
+  skip()
+})

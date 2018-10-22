c8d223431779b6722d5e15ce20194921d4f181ff
Lucas Graybuck <lucasg@alleninstitute.org>
2018-10-21 19:31:56 -0700

    refactored group_dot_plots(), added max_size to control dot size.

--- R/plot_main.R
+++ R/plot_main.R
@@ -1142,7 +1142,6 @@ group_heatmap_plot <- function(data,
                                 group_order, 
                                 "sample_name")
   
-
   gene_stats <- group_stats(gene_data,
                             value_cols = genes,
                             anno = anno,
@@ -1301,6 +1300,7 @@ group_heatmap_plot <- function(data,
 #'   \item "max"
 #'   }
 #' @param size_stat The statistic to apply to each group for scaling dot size. Same options as fill_stat. Default = "prop_gt0".
+#' @param max_size Maximum size of dots, in pts.
 #' @param log_scale Logical , determines if data is log scaled before plotting. Default = FALSE.
 #' @param normalize_rows Logical, whether or not to rescale data within each row of the plot. Default = FALSE.
 #' @param font_size numeric object, the font size (in pts) used to make the plot.
@@ -1308,6 +1308,7 @@ group_heatmap_plot <- function(data,
 #' @param show_counts Logical, whether or not to display sample counts at the top of labels. Default = TRUE.
 #' @param rotate_counts Logical, whether or not to rotate sample counts by 90 degrees. Default = FALSE.
 #' @param max_width numeric object, percent of plot width that should be used for maximum expression values (0 to 100). Default is 10.
+#' @param return_type What values to return - can be "plot", "data", or "both". Default is "plot".
 #' 
 #' @return a ggplot2 plot object
 #'
@@ -1318,6 +1319,7 @@ group_dot_plot <- function(data,
                            group_order = NULL,
                            fill_stat = "median",
                            size_stat = "prop_gt0",
+                           max_size = 10,
                            log_scale = TRUE,
                            normalize_rows = FALSE,
                            colorset = NULL,
@@ -1325,37 +1327,39 @@ group_dot_plot <- function(data,
                            label_height = 25,
                            show_counts = TRUE, 
                            rotate_counts = FALSE,
-                           max_width = 10) {
-  library(dplyr)
-  library(ggplot2)
-  
+                           max_width = 10,
+                           return_type = "plot") {
+  # Reverse so that genes go from top to bottom
+  # instead of bottom to top.
   genes <- rev(genes)
   
-  group_id <- paste0(grouping, "_id")
-  group_label <- paste0(grouping, "_label")
-  group_color <- paste0(grouping, "_color")
+  group_cols <- group_columns(grouping)
   
-  gene_data <- data[,c("sample_name",genes)]
-  gene_data <- gene_data[match(anno$sample_name, data$sample_name),]
-  
-  # Get maximum values for each gene before rescaling to plot space.
-  max_vals <- map_dbl(genes, function(x) { max(gene_data[[x]]) })
-  names(max_vals) <- genes
+  # Filter data to genes and samples in anno
+  gene_data <- filter_gene_data(data, 
+                                genes, 
+                                anno, 
+                                group_cols,
+                                group_order, 
+                                "sample_name")
   
   gene_fill_stats <- group_stats(gene_data,
                                  value_cols = genes,
                                  anno = anno,
-                                 grouping = group_label,
+                                 grouping = group_cols$label,
                                  stat = fill_stat)
+  
+  # Get maximum values for each gene before rescaling to plot space.
+  max_vals_unscaled <- max_gene_vals(gene_fill_stats, genes)
   
   gene_size_stats <- group_stats(gene_data,
                                  value_cols = genes,
                                  anno = anno,
-                                 grouping = group_label,
+                                 grouping = group_cols$label,
                                  stat = size_stat)
   
   if(log_scale) {
-    gene_fill_stats[,genes] <- log10(gene_fill_stats[,genes] + 1)
+    gene_fill_stats <- scale_gene_data(gene_fill_stats, genes, scale_type = "log10")
   }
   
   # Convert the data values to heatmap colors
@@ -1369,75 +1373,44 @@ group_dot_plot <- function(data,
   
   # Left-join data to anno. This will ensure that data is filtered for the cells provided in anno
   plot_anno <- anno %>%
-    select(one_of(group_id, group_label, group_color)) %>%
+    select(one_of(group_cols$id, group_cols$label, group_cols$color)) %>%
     unique()
   
-  group_n <- anno %>%
-    group_by_(group_id) %>%
+  group_counts <- anno %>%
+    group_by_(group_cols$id) %>%
     summarise(group_n = n())
   
   plot_data <- plot_anno %>%
-    left_join(gene_fill_data, by = group_label) %>%
-    left_join(gene_size_stats, by = group_label)
+    left_join(gene_fill_data, by = group_cols$label) %>%
+    left_join(gene_size_stats, by = group_cols$label)
   
-  # Add an x position to each group
-  if(!is.null(group_order)) {
-    # Because we allow ranges, and groups may not necessarily be continuous integer sets
-    # We have to filter out any that don't match first.
-    group_order <- group_order[group_order %in% anno[[group_id]]]
-    
-    group_order_df <- data.frame(group = group_order) %>%
-      mutate(xpos = 1:n())
-    names(group_order_df)[1] <- group_id
-    
-    plot_data <- plot_data %>%
-      filter_(paste0(group_id, " %in% group_order")) %>%
-      left_join(group_order_df, by = group_id) %>%
-      left_join(group_n, by = group_id)
-    
-  } else {
-    # Otherwise, arrange using the group_id for the group_by parameter, and use that order.
-    group_order_df <- plot_data %>%
-      select(one_of(group_id)) %>%
-      arrange_(group_id) %>%
-      mutate(xpos = 1:n())
-    
-    plot_data <- plot_data %>%
-      left_join(group_order_df, by = group_id) %>%
-      left_join(group_n, by = group_id)
-  }
+  # Add x-positions for each group
+  plot_data <- add_group_xpos(plot_data,
+                              group_cols = group_cols,
+                              group_order = group_order)
   
-  n_genes <- length(genes)
-  n_groups <- length(unique(plot_data[[group_id]]))
-  n_samples <- nrow(data)
+  # Compute basic count stats that are used downstream
+  # n_stats$genes, n_stats$samples, and n_stats$groups
+  n_stats <- get_n_stats(plot_data, group_cols, genes)
   
   header_labels <-build_header_labels(data = plot_data, 
                                       grouping = grouping,
                                       group_order = group_order,
-                                      ymin = n_genes + 1, 
+                                      ymin = n_stats$genes + 1, 
                                       label_height = label_height, 
                                       label_type = "simple")
-  
-  # Build the maximum value labels for the right edge
-  max_labels <- data.frame(x = (n_groups + 0.5) * 1.01,
-                           y = 1:n_genes + 0.5,
-                           label = sci_label(max_vals) )
-  max_header <- data.frame(x = (n_groups + 0.5) * 1.01,
-                           y = n_genes + 1,
-                           label = "Max value")
-  max_width <- n_groups*(max_width/100)/(1-max_width/100)
   
   label_y_size <- max(header_labels$ymax) - min(header_labels$ymin)
   
   group_data <- plot_data %>%
     select(xpos, group_n) %>%
-    mutate(label_y = n_genes + label_y_size * 0.05,
+    mutate(label_y = n_stats$genes + label_y_size * 0.05,
            group_n_y = max(header_labels$ymax) - 0.1 * label_y_size)
   
   # Plot setup
   p <- ggplot() +
     scale_fill_identity() +
-    scale_size_area() +
+    scale_size_area(max_size = pt2mm(max_size)) +
     scale_y_continuous("", 
                        breaks = 1:length(genes) + 0.45, 
                        labels = genes, 
@@ -1449,9 +1422,9 @@ group_dot_plot <- function(data,
           axis.text.x = element_blank(),
           axis.ticks.x = element_blank(),
           legend.position = "none") +
-    geom_hline(aes(yintercept = 1:(n_genes)), size = 0.2)
+    geom_hline(aes(yintercept = 1:(n_stats$genes)), size = 0.2)
   
-  # plot the swarms for each gene
+  # plot the dots for each gene
   for(i in 1:length(genes)) {
     gene <- genes[[i]]
     gene_fill <- paste0(gene, "_fill")
@@ -1468,38 +1441,26 @@ group_dot_plot <- function(data,
   }
   
   # Cluster labels
-  p <- p +
-    geom_rect(data = header_labels, 
-              aes(xmin = xmin, ymin = ymin, 
-                  xmax = xmax, ymax = ymax, 
-                  fill = color)) +
-    geom_text(data = header_labels,
-              aes(x = (xmin + xmax) / 2, 
-                  y = ymin + 0.05, 
-                  label = label),
-              angle = 90, 
-              vjust = 0.35, hjust = 0, 
-              size = pt2mm(font_size)) +
-    # Maximum value labels on right side of plot
-    geom_rect(aes(xmin = n_groups + 0.5, xmax = n_groups + 0.5 + max_width, 
-                  ymin = 1, ymax = max(header_labels$ymax)),
-              fill = "white") +
-    geom_text(data = max_header,
-              aes(x = x, y = y, 
-                  label = label),
-              angle = 90, 
-              hjust = 0, vjust = 0.35, 
-              size = pt2mm(font_size)) +
-    geom_text(data = max_labels,
-              aes(x = x, y = y, 
-                  label = label),
-              hjust = 0, vjust = 0.35, 
-              size = pt2mm(font_size), 
-              parse = TRUE)
+  p <- ggplot_header_labels(p,
+                            header_labels = header_labels,
+                            header_polygons = NULL,
+                            font_size = font_size)
+  
+  ### Maximum value labels at the right edge of the plot
+  max_val_dfs <- build_max_dfs(n_stats, 
+                               width_stat = "groups", 
+                               max_vals_unscaled, 
+                               max_width)
+  
+  p <- ggplot_max_vals(p,
+                       n_stats = n_stats,
+                       width_stat = "groups",
+                       max_val_dfs = max_val_dfs,
+                       font_size = font_size)
   
   # Cluster counts
-  if(show_counts) {
-    if(rotate_counts) {
+  if (show_counts) {
+    if (rotate_counts) {
       p <- p + geom_text(data = group_data,
                          aes(x = xpos,
                              y = group_n_y, 
@@ -1516,7 +1477,22 @@ group_dot_plot <- function(data,
     }
   }
   
-  return(p)
+  if(return_type == "plot") {
+    return(p)
+  } else if(return_type == "data") {
+    return(list(plot_data = plot_data,
+                header_labels = header_labels,
+                header_polygons = header_polygons,
+                max_val_dfs = max_val_dfs,
+                n_stats = n_stats))
+  } else if(return_type == "both") {
+    return(list(plot = p,
+                plot_data = plot_data,
+                header_labels = header_labels,
+                group_counts = group_data,
+                max_val_dfs = max_val_dfs,
+                n_stats = n_stats))
+  }
 }
 
 #' Build a heatmap legend plot
--- testing/build_demo_plots.R
+++ testing/build_demo_plots.R
@@ -9,72 +9,73 @@ data_df <- cbind(sample_name = colnames(data),
                  as.data.frame(t(data[c("Pvalb","Sst","Rorb"),])))
 
 sample_bar_plot(data_df, 
-                 anno, 
-                 genes = c("Pvalb","Sst","Rorb"), 
-                 grouping = "primary_type", 
-                 log_scale = FALSE,
-                 font_size = 5)
+                anno, 
+                genes = c("Pvalb","Sst","Rorb"), 
+                grouping = "primary_type", 
+                log_scale = FALSE,
+                font_size = 5)
 ggsave("man/figures/sample_bar_plot.png", width = 4, height = 2.5)
 
 sample_fire_plot(data_df, 
-                  anno, 
-                  genes = c("Pvalb","Sst","Rorb"), 
-                  grouping = "primary_type", 
-                  log_scale = TRUE,
-                  top_value = "lowest",
-                 font_size = 5)
-ggsave("man/figures/sample_fire_plot.png", width = 4, height = 2.5)
-
-sample_heatmap_plot(data_df, 
                  anno, 
                  genes = c("Pvalb","Sst","Rorb"), 
                  grouping = "primary_type", 
                  log_scale = TRUE,
+                 top_value = "lowest",
                  font_size = 5)
-ggsave("man/figures/sample_heatmap_plot.png", width = 4, height = 2.5)
+ggsave("man/figures/sample_fire_plot.png", width = 4, height = 2.5)
 
-group_dot_plot(data_df, 
+sample_heatmap_plot(data_df, 
                     anno, 
                     genes = c("Pvalb","Sst","Rorb"), 
                     grouping = "primary_type", 
                     log_scale = TRUE,
-                    font_size = 5,
+                    font_size = 5)
+ggsave("man/figures/sample_heatmap_plot.png", width = 4, height = 2.5)
+
+group_dot_plot(data_df, 
+               anno, 
+               genes = c("Pvalb","Sst","Rorb"), 
+               grouping = "primary_type", 
+               log_scale = TRUE,
+               font_size = 5,
+               max_size = 8,
                rotate_counts = TRUE)
 ggsave("man/figures/group_dot_plot.png", width = 4, height = 2.5)
 
 group_violin_plot(data_df, 
-               anno, 
-               genes = c("Pvalb","Sst","Rorb"), 
-               grouping = "primary_type", 
-               log_scale = FALSE,
-               font_size = 5,
-               rotate_counts = TRUE)
-ggsave("man/figures/group_violin_plot.png", width = 4, height = 2.5)
-
-group_quasirandom_plot(data_df, 
                   anno, 
                   genes = c("Pvalb","Sst","Rorb"), 
                   grouping = "primary_type", 
                   log_scale = FALSE,
                   font_size = 5,
                   rotate_counts = TRUE)
+ggsave("man/figures/group_violin_plot.png", width = 4, height = 2.5)
+
+group_quasirandom_plot(data_df, 
+                       anno, 
+                       genes = c("Pvalb","Sst","Rorb"), 
+                       grouping = "primary_type", 
+                       log_scale = FALSE,
+                       font_size = 5,
+                       rotate_counts = TRUE)
 ggsave("man/figures/group_quasirandom_plot.png", width = 4, height = 2.5)
 
 group_box_plot(data_df, 
-                  anno, 
-                  genes = c("Pvalb","Sst","Rorb"), 
-                  grouping = "primary_type", 
-                  log_scale = FALSE,
-                  font_size = 5,
+               anno, 
+               genes = c("Pvalb","Sst","Rorb"), 
+               grouping = "primary_type", 
+               log_scale = FALSE,
+               font_size = 5,
                rotate_counts = TRUE)
 ggsave("man/figures/group_box_plot.png", width = 4, height = 2.5)
 
 group_heatmap_plot(data_df, 
-               anno, 
-               genes = c("Pvalb","Sst","Rorb"), 
-               grouping = "primary_type", 
-               stat = "tmean",
-               log_scale = TRUE,
-               font_size = 5,
-               rotate_counts = TRUE)
+                   anno, 
+                   genes = c("Pvalb","Sst","Rorb"), 
+                   grouping = "primary_type", 
+                   stat = "tmean",
+                   log_scale = TRUE,
+                   font_size = 5,
+                   rotate_counts = TRUE)
 ggsave("man/figures/group_heatmap_plot.png", width = 4, height = 2.5)
